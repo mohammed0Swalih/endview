e666fd0f4f491c25842a18f29c6e4f930cb6b5a7
Swalih Mohammed <swalih3409@gmail.com>
2026-06-22 14:45:06 +0400

    authentication and stroge implemented

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,10 @@
+import { redirect } from 'next/dist/client/components/navigation';
 import { ReactNode } from 'react'
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
-
-const AuthLayout = ({ children }: { children: ReactNode }) => {
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+    if (isUserAuthenticated) redirect("/"); // If user is authenticated and try to access signin page from homepage redirect to home page itself
   return (
     <div className="auth-layout">
       {children}
